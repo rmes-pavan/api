05c5460fb6a1dd5797539b35c595d2cd8b10e370
rmes-pavan <92154998+rmes-pavan@users.noreply.github.com>
2021-10-29 19:18:15 +0530

    Update api.sh

--- api.sh
+++ api.sh
@@ -1,1 +1,12 @@
+Newman run Alarm_api.json --env-var ip=79
+Newman run AssetType.json --env-var ip=79
+Newman run AssetTypeDetails.json --env-var ip=79
+Newman run DataFormat.json --env-var ip=79
+Newman run DeviceDetails.json --env-var ip=79
+Newman run Devices.json --env-var ip=79
+Newman run DeviceType.json --env-var ip=79
+Newman run DeviceTypeDeatils.json --env-var ip=79
+Newman run Gateway.json --env-var ip=79
+Newman run Protocol.json --env-var ip=79
+Newman run Units.json --env-var ip=79
 rm -r api
