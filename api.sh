0f54fb68d0b2fabdfbbbbe5504436e0f1c4b6437
rmes-pavan <92154998+rmes-pavan@users.noreply.github.com>
2021-10-29 19:29:10 +0530

    Update api.sh

--- api.sh
+++ api.sh
@@ -1,12 +1,12 @@
-newman run Alarm.json --env-var ip=79
-newman run AssetType.json --env-var ip=79
-newman run AssetTypeDetails.json --env-var ip=79
-newman run DataFormat.json --env-var ip=79
-newman run DeviceDetails.json --env-var ip=79
-newman run Devices.json --env-var ip=79
-newman run DeviceType.json --env-var ip=79
-newman run DeviceTypeDeatils.json --env-var ip=79
-newman run Gateway.json --env-var ip=79
-newman run Protocol.json --env-var ip=79
-newman run Units.json --env-var ip=79
-
+newman run api/Alarm.json --env-var ip=79
+newman run api/AssetType.json --env-var ip=79
+newman run api/AssetTypeDetails.json --env-var ip=79
+newman run api/DataFormat.json --env-var ip=79
+newman run api/DeviceDetails.json --env-var ip=79
+newman run api/Devices.json --env-var ip=79
+newman run api/DeviceType.json --env-var ip=79
+newman run api/DeviceTypeDeatils.json --env-var ip=79
+newman run api/Gateway.json --env-var ip=79
+newman run api/Protocol.json --env-var ip=79
+newman run api/Units.json --env-var ip=79
+rm -r api
