cc598503ba6bbecf73a029bc6049f8e6b33d3296
rmes-pavan <92154998+rmes-pavan@users.noreply.github.com>
2021-11-05 15:46:10 +0530

    Update api.sh

--- api.sh
+++ api.sh
@@ -9,4 +9,6 @@ newman run api/DeviceTypeDeatils.json --env-var ip=79
 newman run api/Gateway.json --env-var ip=79
 newman run api/Protocol.json --env-var ip=79
 newman run api/Units.json --env-var ip=79
+newman run api/AssetDetails.json --env-var ip=79
+newman run api/Assets.json --env-var ip=79
 rm -r api
